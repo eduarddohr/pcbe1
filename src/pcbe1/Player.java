6268636b357da7659bf7fb76d1a5a8935b541ab3
davidmihai98 <davidmihai98@yahoo.com>
2019-10-27 19:48:17 +0200

    continued

--- src/pcbe1/Player.java
+++ src/pcbe1/Player.java
@@ -1,6 +1,7 @@
 package pcbe1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
@@ -10,13 +11,18 @@ public class Player extends Thread{
 	private Map<Objective, Integer> objectives;
 	private ArrayList<Resource> resources;
 	
+	public Player(String name) {
+		this.name=name;
+		this.objectives =  new HashMap<Objective, Integer>();
+		//dam 3 resurse random la inceput la fiecare jucator
+		getRandomResources(3);
+	}
 	private void recieveResource(Resource res){
 		resources.add(res);
 	}
 	
 	private void giveResource(Resource res){
-		resources.remove(resources.indexOf(res));
-		
+		resources.remove(resources.indexOf(res));	
 	}
 	
 	private int calculatePoints(){
@@ -26,7 +32,7 @@ public class Player extends Thread{
 		}
 		return totalPoints;
 	}
-	//asta e practic takeTurn
+	//cred ca asta e practic takeTurn, dar nu stiu :))
 	public void run(){
 		
 		try {
@@ -37,18 +43,18 @@ public class Player extends Thread{
 		}
 	}
 	
-	private void getRandomResources(){ 
+	private void getRandomResources(int nrRes){ 
 		Random r = new Random();
-		for (int nr : r.ints(2, 0, 3).toArray()) {
+		for (int nr : r.ints(nrRes, 0, 3).toArray()) {
 			switch(nr) {
 			case 0:
-				giveResource(new Brick());
+				recieveResource(new Brick());
 				break;
 			case 1:
-				giveResource(new Stone());
+				recieveResource(new Stone());
 				break;
 			case 2:
-				giveResource(new Wood());
+				recieveResource(new Wood());
 				break;
 			}
 		}
