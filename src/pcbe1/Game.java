ec3f2caa46a0f82c7cb6c79fcb5b6d3aaf935e05
eduarddohr <eduarddohr@gmail.com>
2019-11-04 19:14:44 +0200

    grant recources to players at beginning + semaphore on resources

--- src/pcbe1/Game.java
+++ src/pcbe1/Game.java
@@ -4,14 +4,15 @@ import java.nio.channels.NonWritableChannelException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.concurrent.Semaphore;
 
 public class Game {
 
-	public static ArrayList<Resource> bricks;
-	public static ArrayList<Resource> wood;
-	public static ArrayList<Resource> stones;
+	public static ArrayList<Resource> bricks = new ArrayList<Resource>();
+	public static ArrayList<Resource> wood = new ArrayList<Resource>();
+	public static ArrayList<Resource> stones = new ArrayList<Resource>();
 	//lista IMUTABILA cu obiective puse in ordinea importantei lor
-	final public static ArrayList<Objective> objectiveslist = (ArrayList<Objective>) Collections.unmodifiableList(new ArrayList<Objective>(Arrays.asList(new Town(),new Settlement(),new Road()))); 
+	final public static ArrayList<Objective> objectiveslist = new ArrayList<Objective>(Arrays.asList(new Town(),new Settlement(),new Road())); 
 	private ArrayList<Trade> marketplace;
 	final static int nrPlayers = 4;
 	
@@ -19,11 +20,28 @@ public class Game {
 		return true;
 	}
 	
+	public Game() {
+		for(int i = 0; i < 20; i++) {
+			bricks.add(new Brick());
+			wood.add(new Wood());
+			stones.add(new Stone());
+		}
+	}
+	
+
+	
 	public static void main(String[] argv){
-		
+		Game game = new Game();
+		Semaphore semaphore = new Semaphore(1);
 		for(int i = 0; i < nrPlayers; i++) {
-			Player player = new Player("cineva");
+			Player player = new Player(semaphore, "player " + (i+1));
 			player.start();
 		}
+		
+		System.out.println(Game.bricks.size());
+		System.out.println(Game.wood.size());
+		System.out.println(Game.stones.size());
+		
+		
 	}
 }
--- src/pcbe1/Player.java
+++ src/pcbe1/Player.java
@@ -4,18 +4,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 
 public class Player extends Thread{
 	
 	private String name;
+	private Semaphore semaphore;
 	private Map<Objective, Integer> objectives;
-	private ArrayList<Resource> resources;
+	private ArrayList<Resource> resources = new ArrayList<Resource>();
 	
-	public Player(String name) {
+	public Player(Semaphore semaphore, String name) {
+		this.semaphore = semaphore;
 		this.name=name;
 		this.objectives =  new HashMap<Objective, Integer>();
-		//dam 3 resurse random la inceput la fiecare jucator
-		getRandomResources(3);
 	}
 	private void recieveResource(Resource res){
 		resources.add(res);
@@ -36,28 +37,38 @@ public class Player extends Thread{
 	public void run(){
 		
 		try {
+			//semaphore.acquire();
+			getRandomResources(3);
+			//semaphore.release();
 			
+			System.out.println("Hello, me is player and will implement the whole game -" + name);
+
 		}
 		catch(Exception e) {
 			 System.out.println ("Exception is caught"); 
 		}
 	}
-	
-	private void getRandomResources(int nrRes){ 
+	//thread safe
+	private void getRandomResources(int nrRes) throws InterruptedException{ 
+		semaphore.acquire();
 		Random r = new Random();
 		for (int nr : r.ints(nrRes, 0, 3).toArray()) {
 			switch(nr) {
 			case 0:
 				recieveResource(new Brick());
+				Game.bricks.remove(0);
 				break;
 			case 1:
 				recieveResource(new Stone());
+				Game.stones.remove(0);
 				break;
 			case 2:
 				recieveResource(new Wood());
+				Game.wood.remove(0);
 				break;
 			}
 		}
+		semaphore.release();
 	}
 
 }
